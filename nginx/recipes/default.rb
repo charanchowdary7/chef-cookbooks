40b9bc2546df6530ce01e36eb80782997125204f
charan <charanchowdary7@gmail.com>
2017-08-30 05:05:28 +0530

    second commit

---
+++ nginx/recipes/default.rb
@@ -0,0 +1,18 @@
+package 'git'
+package 'tree'
+
+package 'nginx' do
+  action :install
+end
+
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+
+cookbook_file "/var/www/html/index.nginx-debian.html" do
+  source "index.html"
+  mode "0644"
+end
+
